1c8bb73dfc6481f9581ccbc1a7d74db02c9e7646
temp <temp>
2016-10-13 12:28:23 -0400

    Add project files.

---
+++ Cash Calculator/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Cash_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.poodleLabel = new System.Windows.Forms.Label();
+            this.pugLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.poodleInput = new System.Windows.Forms.TextBox();
+            this.pugInput = new System.Windows.Forms.TextBox();
+            this.shihtzuInput = new System.Windows.Forms.TextBox();
+            this.tittleLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // poodleLabel
+            // 
+            this.poodleLabel.AutoSize = true;
+            this.poodleLabel.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.poodleLabel.Location = new System.Drawing.Point(32, 88);
+            this.poodleLabel.Name = "poodleLabel";
+            this.poodleLabel.Size = new System.Drawing.Size(78, 33);
+            this.poodleLabel.TabIndex = 0;
+            this.poodleLabel.Text = "Poodle";
+            // 
+            // pugLabel
+            // 
+            this.pugLabel.AutoSize = true;
+            this.pugLabel.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pugLabel.Location = new System.Drawing.Point(32, 186);
+            this.pugLabel.Name = "pugLabel";
+            this.pugLabel.Size = new System.Drawing.Size(51, 33);
+            this.pugLabel.TabIndex = 2;
+            this.pugLabel.Text = "Pug";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 286);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Shih Tzu";
+            // 
+            // poodleInput
+            // 
+            this.poodleInput.Location = new System.Drawing.Point(312, 104);
+            this.poodleInput.Name = "poodleInput";
+            this.poodleInput.Size = new System.Drawing.Size(54, 20);
+            this.poodleInput.TabIndex = 4;
+            // 
+            // pugInput
+            // 
+            this.pugInput.Location = new System.Drawing.Point(312, 199);
+            this.pugInput.Name = "pugInput";
+            this.pugInput.Size = new System.Drawing.Size(54, 20);
+            this.pugInput.TabIndex = 5;
+            // 
+            // shihtzuInput
+            // 
+            this.shihtzuInput.Location = new System.Drawing.Point(312, 296);
+            this.shihtzuInput.Name = "shihtzuInput";
+            this.shihtzuInput.Size = new System.Drawing.Size(54, 20);
+            this.shihtzuInput.TabIndex = 6;
+            // 
+            // tittleLabel
+            // 
+            this.tittleLabel.AutoSize = true;
+            this.tittleLabel.Font = new System.Drawing.Font("Segoe Script", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tittleLabel.Location = new System.Drawing.Point(249, 9);
+            this.tittleLabel.Name = "tittleLabel";
+            this.tittleLabel.Size = new System.Drawing.Size(189, 30);
+            this.tittleLabel.TabIndex = 7;
+            this.tittleLabel.Text = "Pick your Puppy";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Adobe Fangsong Std R", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(225, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "$550.00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Adobe Fangsong Std R", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(228, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "$270.00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Adobe Fangsong Std R", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(231, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "$325.00";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::Cash_Calculator.Properties.Resources.Pug_puppy;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(148, 171);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(74, 67);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::Cash_Calculator.Properties.Resources.black_she_poo;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(148, 260);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(71, 71);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Cash_Calculator.Properties.Resources.brown_poodle;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(148, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 71);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.calculateButton.Location = new System.Drawing.Point(303, 340);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(80, 33);
+            this.calculateButton.TabIndex = 13;
+            this.calculateButton.Text = "Check out";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(437, 75);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 13);
+            this.outputLabel.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(645, 385);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tittleLabel);
+            this.Controls.Add(this.shihtzuInput);
+            this.Controls.Add(this.pugInput);
+            this.Controls.Add(this.poodleInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pugLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.poodleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pick a Puppy";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label poodleLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label pugLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox poodleInput;
+        private System.Windows.Forms.TextBox pugInput;
+        private System.Windows.Forms.TextBox shihtzuInput;
+        private System.Windows.Forms.Label tittleLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Cash Calculator/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cash_Calculator
+{
+    public partial class Form1 : Form
+    {
+        const double POODLE_COST = 550.00;
+        const double PUG_COST = 270.00;
+        const double SHITTZU_COST = 325.00;
+        const double FIRSTSHOTS_COST = 100.00;
+        const double TAX_COST = 0.13;
+        int poodle = 0;
+        int pug = 0;
+        int shihtzu = 0;
+        int change;
+        double price;
+        double tax;
+        double pricewithTax;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // number in text box into diameter equation
+                poodle = Convert.ToInt16(poodleInput.Text);
+                pug = Convert.ToInt16(pugInput.Text);
+                shihtzu = Convert.ToInt16(shihtzuInput.Text);
+                pricewithTax = Convert.ToInt16(Text);
+
+                tax = price * TAX_COST;
+               
+                
+
+                //get inputs from screen and put into poodle, pug, and  ...
+
+                price = (poodle * POODLE_COST) + (pug * PUG_COST) + (shihtzu * SHITTZU_COST) + (poodle + pug + shihtzu * FIRSTSHOTS_COST) * TAX_COST;
+
+                outputLabel.Text = price.ToString("C");
+
+            }
+            catch
+            { 
+                   outputLabel.Text = " please put a number in every space";
+            }
+
+        }
+    }
+}
